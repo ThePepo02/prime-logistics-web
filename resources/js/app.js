050475f7796669f210e2e4ef31321d86cd60976b
raulh <raulhernandezg2001@gmail.com>
2026-04-07 17:53:24 +0200

    ultimos cambios hechos

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,20 @@
 import './bootstrap';
+import { createApp } from 'vue/dist/vue.esm-bundler.js';
+import DashboardAdminComponent from './components/admin/DashboardAdminComponent.vue';
+
+const app = createApp({});
+
+
+
+
+
+
+app.component('dashboardAdmin-component', DashboardAdminComponent);
+
+
+
+
+
+
+
+app.mount('#app');
---
+++ resources/views/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.main')
+
+@section('content')
+    <dashboardAdmin-component></dashboardAdmin-component>
+@endsection
