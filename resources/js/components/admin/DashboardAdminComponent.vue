8cfa5bfee8bbfbed411629e6a68af7d9cd038c60
raulh <raulhernandezg2001@gmail.com>
2026-04-07 17:42:16 +0200

    cambios en dashBoardAdminComponent.vue hechos

--- resources/js/components/admin/DashboardAdminComponent.vue
+++ resources/js/components/admin/DashboardAdminComponent.vue
@@ -2,22 +2,46 @@
     <div>
         <!-- encabezado -->
         <header>
-         <div class="logo"></div>
-         <title>Dashboard Admin</title>
+            <div class="logo"></div>
+            <title>Dashboard Admin</title>
         </header>
-         <!-- Dashboard -->
-          <div id="dashboard">
+        <main>
             <h1>Dashboard</h1>
-            <div class="menu"></div>
+            <nav>
+                <div class="administracion">
+                    <h2>Administración</h2>
+                    <ul>
+                        <li><a href="">Dashboard</a></li>
+                        <li><a href="">Gestión de Usuarios</a></li>
+                    </ul>
+                </div>
+                <div class="operaciones">
+                    <h2>Operaciones</h2>
+                    <ul>
+                        <li><a href="">Todas las Ofertas</a></li>
+                        <li><a href="">Operaciones Activas</a></li>
+                    </ul>
+                </div>
+                <div class="sistema">
+                    <h2>Sistema</h2>
+                    <ul>
+                        <li><a href="">Datos Maestros</a></li>
+                        <li><a href="">Configuración</a></li>
+                    </ul>
+                </div>
+            </nav>
             <button type="button" class="btn_exportarDatos">
-                <img src="" alt="">Exportar datos
+                <img src="/public/iconoExportarDatos.png" alt="Icono exportar datos">Exportar datos
             </button>
             <button type="button">
                 <a href="">
                     <img src="/public/notificaciones-logo.png" alt="Notificaciones logo">
                 </a>
             </button>
-          </div>
+            <div id="iconoPerfilUsuarioAdmin">
+                <img src="/public/perfilUsuarioAdmin.png" alt="Perfil Usuario administrador">
+            </div>
+        </main>
     </div>
 </template>
 
@@ -25,6 +49,4 @@
 
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
