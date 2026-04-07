70d2c7bd598edc259b7a8df53a81983caa7d817f
raulh <raulhernandezg2001@gmail.com>
2026-04-07 17:09:09 +0200

    componente dashboard de admin

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,3 +2,7 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
---
+++ resources/js/components/admin/DashboardAdminComponent.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <!-- encabezado -->
+        <header>
+         <div class="logo"></div>
+         <title>Dashboard Admin</title>
+        </header>
+         <!-- Dashboard -->
+          <div id="dashboard">
+            <h1>Dashboard</h1>
+            <div class="menu"></div>
+            <button type="button" class="btn_exportarDatos">
+                Exportar datos
+            </button>
+            <button type="button">
+                <a href="">
+                    <img src="" alt="">
+                </a>
+            </button>
+          </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
