1e5762eefad01edb87691a32f58285ace0ab7fa1
Anthony Paz Coral <161304780+ThePepo02@users.noreply.github.com>
2026-04-09 15:31:25 +0200

    creacion del app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
 import './bootstrap';
+
+import { createApp } from 'vue/dist/vue.esm-bundler.js';
+
+
+const app = createApp({});
+
+// Los nombres de los componentes deben coincidir con los nombres de los archivos .vue tipo ('clicles-components', CiclesComponents)
+// Luego arriba en justo debajo del createApp se agrega import CiclesComponents from './components/CiclesComponents.vue';
+
+app.component('');
+
+
+// Luego se mosntan los componentes en algun sitio del blade usando el app-amount('');
