3164346ad4a9712f2996dadc706b25c723008707
raulh <raulhernandezg2001@gmail.com>
2026-04-09 17:58:14 +0200

    ruta

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -4,4 +4,4 @@ window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 const url = import.meta.env.VITE_APP_URL;
-window.axios.defaults.baseURL=`${url}/api/`;
+window.axios.defaults.baseURL=`${url}`;
