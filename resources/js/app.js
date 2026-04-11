e83d329d8e16695db0536b339cf4d64818f3c202
Angel Luis Pérez Jiménez <aperezj2122@politecnics.barcelona>
2026-04-11 21:09:17 +0200

    feat: SidebarOperador reutilizable con navegación completa

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,20 +1,19 @@
 import './bootstrap';
 import { createApp } from 'vue/dist/vue.esm-bundler.js';
+
+// Componentes Admin (compañero)
 import DashboardAdminComponent from './components/admin/DashboardAdminComponent.vue';
+
+// Componentes Operador (tú)
+import SidebarOperador from './components/operador/SidebarOperador.vue';
+
 
 const app = createApp({});
 
-
-
-
-
-
+// Admin
 app.component('dashboardAdmin-component', DashboardAdminComponent);
 
-
-
-
-
-
+// Operador
+app.component('sidebar-operador', SidebarOperador);
 
 app.mount('#app');
