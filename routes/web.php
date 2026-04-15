20fa4a77ec18992f23ada9a2ab030c30defb2515
raulh <raulhernandezg2001@gmail.com>
2026-04-15 12:35:20 +0200

    código de api.php y web.php recibido

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
-<<<<<<< HEAD
-});
-=======
 });
 
 Route::get('/dashboard', function () {
     return view('operador');
 });
->>>>>>> main
