ec58fe836e797894ea1bf49618fd48083d5172dd
Anthony Paz Coral <161304780+ThePepo02@users.noreply.github.com>
2026-04-16 16:50:29 +0200

    fix: conflicto resuelto en main.blade.php

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,24 +1,15 @@
 <!DOCTYPE html>
-<html lang="en">
-
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <<<<<<<<< Temporary merge branch 1 <title>@yield('title', 'Prime Logistics Web')</title>
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-        =========
-        <title>
-            @yield('title', 'Prime Logistics Web')
-        </title>
-        @vite(['resources/js/app.js', 'resources/css/app.css'])
-        >>>>>>>>> Temporary merge branch 2
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>@yield('title', 'Prime Logistics Web')</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-
 <body>
     <div id="app">
         @yield('content')
     </div>
 </body>
-
 </html>
