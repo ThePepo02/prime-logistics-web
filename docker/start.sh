9bba4e3d45525f2ad0daa89086b6d6a3ac727088
Angel Luis Pérez Jiménez <aperezj2122@politecnics.barcelona>
2026-04-17 13:40:22 +0200

    demo: presentacion evaluacion

--- docker/start.sh
+++ docker/start.sh
@@ -3,7 +3,9 @@
 # Generar APP_KEY si no existe
 php artisan key:generate --force
 
-# Limpiar y cachear configuración
+# Limpiar y cachear configuración son optimizaciones de rendimiento.
+# Hace que laravel no los leas y los guardamos en el cache,
+# lo que mejora el rendimiento al evitar la necesidad de cargar y procesar los archivos de configuración en cada solicitud.
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
