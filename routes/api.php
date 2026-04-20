b7b93c564788d18ff748d9277c39725cd4c71d85
raulh <raulhernandezg2001@gmail.com>
2026-04-20 16:46:43 +0200

    comentario del sitio donde pondré las rutas API a los controladores correspondientes de la parte del admin

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ Route::middleware('auth:sanctum')->group(function () {
         return $request->user();
     });
 
-    //Gestion Usuarios Administrador
+    //Datos del administrador (dashboard, usuarios, datos maestros)
 
     // Dashboard Operador
     Route::get('/dashboard/stats', [DashboardOperadorController::class, 'stats']);
