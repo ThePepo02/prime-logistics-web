e53ed5f6a9f2ce69adbdbc8da38b9b75a6e21817
acanovas33 <albert.canovas33@gmail.com>
2026-04-22 19:58:35 +0200

    merge

--- routes/api.php
+++ routes/api.php
@@ -1,26 +1,5 @@
 <?php
 
-<<<<<<< HEAD
-use App\Http\Controllers\Client\DashboardController;
-use App\Http\Controllers\Client\OrderController;
-use App\Http\Controllers\Client\TrackingController;
-use App\Http\Controllers\Client\NotificationController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-Route::prefix('client')->group(function () {
-    Route::get('/dashboard', [DashboardController::class, 'index']);
-    Route::get('/orders', [OrderController::class, 'index']);
-    Route::post('/orders', [OrderController::class, 'store']);
-    Route::get('/tracking', [TrackingController::class, 'show']);
-    Route::get('/notifications', [NotificationController::class, 'index']);
-    Route::post('/accept-offer', [NotificationController::class, 'accept']);
-    Route::post('/reject-offer', [NotificationController::class, 'reject']);
-=======
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ClientesController;
 use App\Http\Controllers\DashboardAdminController;
@@ -41,6 +20,22 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
+
+
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::prefix('client')->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index']);
+    Route::get('/orders', [OrderController::class, 'index']);
+    Route::post('/orders', [OrderController::class, 'store']);
+    Route::get('/tracking', [TrackingController::class, 'show']);
+    Route::get('/notifications', [NotificationController::class, 'index']);
+    Route::post('/accept-offer', [NotificationController::class, 'accept']);
+    Route::post('/reject-offer', [NotificationController::class, 'reject']);
+});
 
     //Datos del administrador (dashboard, usuarios, datos maestros)
 
@@ -88,5 +83,4 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/notificacions', [NotificacionsController::class, 'index']);
     Route::put('/notificacions/marcar-totes', [NotificacionsController::class, 'marcarTotes']);
     Route::put('/notificacions/{id}/llegir', [NotificacionsController::class, 'marcarLlegida']);
->>>>>>> ea23f4298696ad98487a64ea3b4adcb5d0cd246b
 });
