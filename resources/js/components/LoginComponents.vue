c750b56d9610741c174351965f9b3045e2ff1048
raulh <raulhernandezg2001@gmail.com>
2026-04-24 16:00:29 +0200

    LoginComponents-methods acceso a /admin añadido

--- resources/js/components/LoginComponents.vue
+++ resources/js/components/LoginComponents.vue
@@ -100,6 +100,8 @@ export default {
                     window.location.href = '/operador';
                 } else if (rolId == 3) {
                     window.location.href = '/cliente';
+                } else if (rolId === 4) {
+                    window.location.href = '/admin';
                 } else {
                     this.error = 'Rol de usuario desconocido';
                 }
